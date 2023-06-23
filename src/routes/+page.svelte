05badc5af210e0262a10f5ff13f8eb29d0c061ff
Sourav Singh <commitsovercoffee@gmail.com>
2023-06-23 14:03:13 +0530

    creates contextMenu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,34 @@
-Hello !
+<script>
+	let menuOptions = ['Add', 'Edit', 'Delte'];
+	let menu = false;
+	let menuPos = { x: 0, y: 0 };
+
+	function handleRightClick(event) {
+		menu = true;
+		console.log('right click happened !');
+		console.log(event.clientX, event.clientY);
+
+		menuPos.x = event.clientX;
+		menuPos.y = event.clientY;
+	}
+</script>
+
+<svelte:window
+	on:contextmenu|preventDefault={handleRightClick}
+	on:click={() => {
+		menu = false;
+	}}
+/>
+
+<section class="mx-auto max-w-prose bg-red-100 h-screen">
+	{#if menu}
+		<ul
+			style="top:{menuPos.y}px; left:{menuPos.x}px"
+			class="absolute rounded-xl bg-lime-100 prose w-40"
+		>
+			{#each menuOptions as opt}
+				<li class="p-2 hover:bg-lime-300">{opt}</li>
+			{/each}
+		</ul>
+	{/if}
+</section>
