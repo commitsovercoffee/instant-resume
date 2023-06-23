826096d77e20c5583544020779559955d4e12676
Sourav Singh <commitsovercoffee@gmail.com>
2023-06-23 20:11:45 +0530

    implements delete opt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,108 @@
 <script>
-	let menuOptions = ['Add', 'Edit', 'Delte'];
+	let menuOptions = ['Add', 'Edit', 'Delete'];
 	let menu = false;
 	let menuPos = { x: 0, y: 0 };
 
+	let skills = ['js', 'html', 'css'];
+	let interests = ['linux', 'sketching', 'boating'];
+
+	let selectedParent = '';
+	let selectedAction = '';
+	let selectedChild = '';
+
 	function handleRightClick(event) {
 		menu = true;
-		console.log('right click happened !');
-		console.log(event.clientX, event.clientY);
-
 		menuPos.x = event.clientX;
 		menuPos.y = event.clientY;
+	}
+
+	function handleMenuClick() {
+		console.log(selectedAction, selectedChild, 'from/to', selectedParent);
+		if (selectedParent === 'skills') {
+			if (selectedAction === 'Delete') {
+				const index = skills.indexOf(selectedChild);
+				if (index > -1) {
+					// only splice array when item is found
+					skills.splice(index, 1); // 2nd parameter means remove one item only
+					skills = skills;
+				}
+			}
+
+			console.log(skills);
+		}
+
+		if (selectedParent === 'interests') {
+			if (selectedAction === 'Delete') {
+				const index = interests.indexOf(selectedChild);
+				if (index > -1) {
+					// only splice array when item is found
+					interests.splice(index, 1); // 2nd parameter means remove one item only
+					interests = interests;
+				}
+			}
+
+			console.log(interests);
+		}
 	}
 </script>
 
 <svelte:window
-	on:contextmenu|preventDefault={handleRightClick}
+	on:contextmenu|preventDefault
 	on:click={() => {
 		menu = false;
 	}}
+	on:contextmenu|preventDefault={handleRightClick}
 />
+{#if menu}
+	<ul
+		style="top:{menuPos.y}px; left:{menuPos.x}px"
+		class="absolute rounded-xl bg-lime-100 prose w-40 z-10"
+	>
+		{#each menuOptions as opt}
+			<li
+				class="p-2 hover:bg-lime-300"
+				on:click={() => {
+					selectedAction = opt;
+					handleMenuClick();
+				}}
+			>
+				{opt}
+			</li>
+		{/each}
+	</ul>
+{/if}
 
-<section class="mx-auto max-w-prose bg-red-100 h-screen">
-	{#if menu}
-		<ul
-			style="top:{menuPos.y}px; left:{menuPos.x}px"
-			class="absolute rounded-xl bg-lime-100 prose w-40"
+<div class="flex justify-between">
+	<ul class="absolute rounded-xl prose w-40 m-4">
+		<h3>Skills</h3>
+		{#each skills as skill}
+			<li
+				bind:innerText={skill}
+				contenteditable="true"
+				on:contextmenu={() => {
+					selectedParent = 'skills';
+					selectedChild = skill;
+				}}
+				class="p-2 bg-sky-300 w-min rounded-xl"
+			>
+				{skill}
+			</li>
+		{/each}
+	</ul>
+</div>
+<ul>
+	<h3>Interests</h3>
+	{#each interests as interest}
+		<li
+			bind:innerText={interest}
+			contenteditable="true"
+			on:contextmenu={() => {
+				selectedParent = 'interests';
+				selectedChild = interest;
+			}}
+			class="p-2 bg-sky-300 w-min rounded-xl"
 		>
-			{#each menuOptions as opt}
-				<li class="p-2 hover:bg-lime-300">{opt}</li>
-			{/each}
-		</ul>
-	{/if}
-</section>
+			{interest}
+		</li>
+	{/each}
+</ul>
